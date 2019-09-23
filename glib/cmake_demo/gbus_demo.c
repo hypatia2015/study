7702952a14b2df1fcd903d1637b7a7b61e730c5e
root <root@localhost.localdomain>
2019-09-23 16:55:22 +0800

    add git version to source

--- glib/cmake_demo/gbus_demo.c
+++ glib/cmake_demo/gbus_demo.c
@@ -23,16 +23,26 @@ void on_name_lost (GDBusConnection *connection,
 {
     printf("on_name_lost has been invoked\n");
 }
- 
+
+void get_version()
+{
+  printf("GIT_VER:%s\n GIT_DATE%s\n BUILD_DATE%s\n",
+    G_STRINGIFY(GIT_VERSION), G_STRINGIFY(GIT_DATETIME),
+     G_STRINGIFY(BUILD_DATETIME));
+} 
 
 int
 main(int agrc, char **argv)
 {
   int ret = 0;
+#ifdef XXX
   int bus_id = 0;
+#endif
   GMainLoop *p_loop = NULL;
 
   p_loop = g_main_loop_new(NULL, FALSE);
+
+#ifdef XXX
   bus_id = g_bus_own_name(G_BUS_TYPE_SYSTEM, "org.test.louting",
     G_BUS_NAME_OWNER_FLAGS_NONE,
     on_bus_acquired,
@@ -45,6 +55,8 @@ main(int agrc, char **argv)
   printf("%d\n", bus_id);
 
   g_main_loop_run(p_loop);
+#endif
 
+  get_version();
   return ret;
 }
